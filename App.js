6f069765ad0f62e70333c294b8b9e56e7a139075
rpotla <rajesh.potla@gmail.com>
2023-11-16 16:51:55 -0500

    episode-01

---
+++ App.js
@@ -0,0 +1,21 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Welcome to First React Page!"
+// );
+// console.log(heading); //object
+
+//nested element
+const nestedEle = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(nestedEle);
